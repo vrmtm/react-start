048bfc86a94611ef0a3a48bff0867364d44f547d
Valdas Pauliukonis <valdas.pauliukonis@flowforma.com>
2019-03-18 16:15:25 +0200

    .

---
+++ src/components/App/App.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import styles from '../../styles/main.scss';
+
+const content = 'Hello world!';
+
+class App extends Component {
+  render() {
+    return <p>{content}</p>;
+  }
+}
+
+export default App;
